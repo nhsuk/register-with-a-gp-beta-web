f7109866e33ffcc39ba0d09aff733896d42e127d
Josh Rowe <josh@s-block.com>
2017-03-09 15:50:17 +0000

    test function on summary page

---
+++ src/__tests__/plugins/register-form/summary-step.js
@@ -0,0 +1,13 @@
+import {renderTemplate, emailGP} from '../../../server/plugins/register-form/steps/summary';
+
+
+describe('summary step', () => {
+
+  it('should email not error when emailing text', () => {
+    return emailGP('Test email').then((args) => {
+      expect(args.ewsFunction).toBe('CreateItem');
+      expect(args.ewsArgs.Items.Message.Body.$value).toBe('Test email');
+    });
+  });
+
+});
