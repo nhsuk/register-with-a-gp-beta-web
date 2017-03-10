00ff9409cfda52801e9e59a87b8ab448f0c16d6a
Josh Rowe <josh@s-block.com>
2017-03-10 12:35:52 +0000

    Add proxy endpoint for address lookup via getaddress.io  - add env vars for address api key and host  - create plugin and rout for address lookup  - add natuarl sort for sorting addresses

---
+++ src/server/plugins/getaddressio/index.js
@@ -0,0 +1,64 @@
+import _ from 'lodash';
+import https from 'https';
+import cache from '../../config/cache';
+import cookies from '../../config/cookies';
+import naturalSort from 'javascript-natural-sort';
+
+
+function getAddresses(postcode) {
+  return new Promise((resolve, reject) => {
+    https.get({
+      host: process.env.POSTCODE_API_HOST,
+      path: `/v2/uk/${ postcode }/?api-key=${ process.env.POSTCODE_API_KEY }&format=true`
+    }, function(response) {
+      if (response.statusCode === 200 && response.statusMessage === 'OK') {
+        let body = '';
+        response.on('data', (d) => {
+          body += d;
+        });
+        response.on('end', () => {
+          const addresses = JSON.parse(body).Addresses;
+          addresses.sort(naturalSort);
+          resolve(addresses);
+        });
+      } else {
+        reject();
+      }
+    });
+  });
+}
+
+
+function addressLookuptHandler(request, reply) {
+  getAddresses(request.url.query.postcode)
+    .then(addresses => {
+      reply(addresses);
+    })
+    .catch(err => {
+      request.log(['error'], err);
+      reply([]);
+    });
+}
+
+
+exports.register = function(server, options, next) {
+  const statelessConfig = {
+    state: cookies.disableCookies,
+    cache: cache.cacheable
+  };
+
+  server.route({
+    method: 'GET',
+    config: _.merge({}, statelessConfig, {id: 'address'}),
+    path: '/address',
+    handler: addressLookuptHandler
+  });
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'AddressLookupRoute',
+  version: '1.0.0',
+  dependencies: 'NunjucksConfig'
+};
