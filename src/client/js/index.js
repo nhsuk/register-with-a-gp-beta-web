343d84cc64a0569138e9e1bed13859aa515a96b5
Dom Smith <dom@teneightfive.com>
2017-03-13 13:22:05 +0000

    Add initial form components
    This adds the nunjucks component and sass for some initial form
    components that will be needed:
    
    - textbox
    - multiple choice (radio/checkbox)

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,5 +1,9 @@
+const labelFocus = require('./modules/label-focus');
+const labelSelect = require('./modules/label-select');
 import title from './lib/title';
 
+labelFocus.init();
+labelSelect.init();
 title.init();
 
 if (module.hot) {
