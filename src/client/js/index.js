ffb19f3c0ff6aa0b1178dc004ebe57ebd4936506
Dom Smith <dom@teneightfive.com>
2017-03-15 15:21:46 +0000

    Add initial error summary component
    This adds a component to show a summary of errors for a step

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,9 +1,11 @@
 const labelFocus = require('./modules/label-focus');
 const labelSelect = require('./modules/label-select');
+const errorSummary = require('./modules/error-summary');
 import title from './lib/title';
 
 labelFocus.init();
 labelSelect.init();
+errorSummary.init();
 title.init();
 
 if (module.hot) {
