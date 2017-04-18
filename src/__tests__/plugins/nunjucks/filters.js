09d41d3d8d31ff7faf0f54c20a427018019211d0
Tamas <tamas.marin@miqsolutions.co.uk>
2017-04-18 09:31:46 +0100

    prepare test function for nunjucks filter

--- src/__tests__/plugins/nunjucks/filters.js
+++ src/__tests__/plugins/nunjucks/filters.js
@@ -1,10 +1,10 @@
-import Filters from '../../../server/plugins/nunjucks/filters';
+import getFilters from '../../../server/plugins/nunjucks/filters';
 
 
 describe('Nunjucks global filters', () => {
 
   it('nl2br should return a string with line breaks', () => {
-    expect(Filters.nl2br('Line one\nline two')).toBe('Line one<br>line two');
+    expect(getFilters.nl2br('Line one\nline two')).toBe('Line one<br>line two');
   });
 
 });
