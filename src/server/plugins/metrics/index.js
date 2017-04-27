62494bf13c5de23a7ea43c1ddcac15a5e38653d0
Tamas <tamas.marin@miqsolutions.co.uk>
2017-04-27 17:11:40 +0100

    indent fix

--- src/server/plugins/metrics/index.js
+++ src/server/plugins/metrics/index.js
@@ -1,29 +1,29 @@
 var Plugin = {};
 Plugin.register = function(server, options, next) {
   server.ext('onRequest', function (request, reply) {
-      request.headers['x-req-start'] = (new Date()).getTime();
-      return reply.continue();
-    });
+    request.headers['x-req-start'] = (new Date()).getTime();
+    return reply.continue();
+  });
   server.ext('onPreResponse', function (request, reply) {
-      if(!request.response.isBoom){
-        var start = parseInt(request.headers['x-req-start']);
-        var end = (new Date()).getTime();
-        request.response
-          .header('x-req-start', start)
-          .header('x-res-end', end)
-          .header('x-response-time', end - start);
-        request.log(['response'],
-          JSON.stringify({
-            url: request.raw.req.url,
-            start: request.response.headers['x-req-start'],
-            end: request.response.headers['x-res-end'],
-           time: request.response.headers['x-response-time'],
-            status: request.response.statusCode
-          })
-        );
-      }
-      return reply.continue();
-    });
+    if(!request.response.isBoom){
+      var start = parseInt(request.headers['x-req-start']);
+      var end = (new Date()).getTime();
+      request.response
+        .header('x-req-start', start)
+        .header('x-res-end', end)
+        .header('x-response-time', end - start);
+      request.log(['response'],
+        JSON.stringify({
+          url: request.raw.req.url,
+          start: request.response.headers['x-req-start'],
+          end: request.response.headers['x-res-end'],
+          time: request.response.headers['x-response-time'],
+          status: request.response.statusCode
+        })
+      );
+    }
+    return reply.continue();
+  });
   next();
 };
 
