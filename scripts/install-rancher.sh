437f78e35a58e4234e1ccedbda298bc01ff7daea
Adam Graves <adam.graves@nhs.net>
2017-04-27 17:03:03 +0100

    :bug: Missed rancher version

--- scripts/install-rancher.sh
+++ scripts/install-rancher.sh
@@ -4,7 +4,7 @@ if [ ! -f "/usr/bin/rancher" ] ; then
   RANCHER_VERSION='v0.6.0-rc2'
 
   echo "Installing Rancher (${RANCHER_VERSION})"
-  curl -L -o /tmp/rancher.tar.gz https://github.com/rancher/cli/releases/download/v0.6.0-rc2/rancher-linux-amd64-${RANCHER_VERSION}.tar.gz
+  curl -L -o /tmp/rancher.tar.gz https://github.com/rancher/cli/releases/download/${RANCHER_VERSION}/rancher-linux-amd64-${RANCHER_VERSION}.tar.gz
 
   mkdir -p /tmp/rancher
   tar -C /tmp/rancher/ -xzf /tmp/rancher.tar.gz
