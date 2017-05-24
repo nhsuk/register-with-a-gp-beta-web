003be92cc9b1d74516d902a3f0891ace81684961
Tamas <tamas.marin@miqsolutions.co.uk>
2017-05-24 15:54:49 +0100

    quote fix

--- src/client/js/modules/disable-button.js
+++ src/client/js/modules/disable-button.js
@@ -3,9 +3,9 @@ const $ = require('jquery');
 class btnDisable{
   init(){
     $('form').submit(function() {
-      $(this).find("button[type='submit']").prop('disabled',true);
+      $(this).find('button[type=\'submit\']').prop('disabled',true);
     });
   }
-  
+
 }
 module.exports = new btnDisable();
