64810eaada37a91de32b59945349d5d31405cb1e
erdem <erdemozkol@gmail.com>
2017-06-14 13:40:11 +0100

    fixed docker-compose.prod issues

--- bin/prod.sh
+++ bin/prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker-compose -p gp-reg -f docker-compose.yml -f docker-compose.prod.yml down -v
-docker-compose -p gp-reg -f docker-compose.yml -f docker-compose.prod.yml up --build --force-recreate "$@"
+docker-compose -p gp-reg -f docker-compose.prod.yml down -v
+docker-compose -p gp-reg -f docker-compose.prod.yml up --build --force-recreate "$@"
