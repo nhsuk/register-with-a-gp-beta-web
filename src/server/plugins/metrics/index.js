8cd788351f782b76fcecb22b3d7bfee4ee75d9e7
Tamas <tamas.marin@miqsolutions.co.uk>
2017-06-20 10:15:45 +0100

    add cors manually to header

--- src/server/plugins/metrics/index.js
+++ src/server/plugins/metrics/index.js
@@ -12,6 +12,8 @@ Plugin.register = function(server, options, next) {
         .header('x-req-start', start)
         .header('x-res-end', end)
         .header('x-response-time', end - start);
+      request.response
+        .header('Access-Control-Allow-Origin', '*');
       request.log(['response'],
         JSON.stringify({
           url: request.raw.req.url,
