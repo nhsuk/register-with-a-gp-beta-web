b05fc61c1b005cd25a2637399209a85bb52bf8b2
Tamas <tamas.marin@miqsolutions.co.uk>
2017-06-29 15:58:39 +0100

    widget page created

---
+++ src/server/plugins/widget/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import cookies from '../../config/cookies';
+import practiceLookup from '../../../shared/lib/practice-lookup';
+
+function widgetHandler(request, reply) {
+  const practice=request.params.practice;
+  if (practice) {
+    const practiceData = practiceLookup.getPractice(request.params.practice);
+    if (typeof practiceData !== 'undefined') {
+      return reply
+        .view('layouts/widget', {
+          practiceData,
+        });
+    }
+  }
+
+  return reply.view(practiceEndTemplate);
+}
+exports.register = function(server, options, next) {
+  const routeConfig = {
+    state: cookies.disableCookies,
+  };
+  server.route({
+    config: _.merge({}, routeConfig, {id: 'widget'}),
+    method: 'GET',
+    path: '/{practice}/widget',
+    handler: widgetHandler
+  });
+  next();
+};
+
+exports.register.attributes = {
+  name: 'widgetRoute',
+  version: '1.0.0',
+  dependencies: 'NunjucksConfig'
+};
