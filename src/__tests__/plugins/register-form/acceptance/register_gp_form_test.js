6a5b082547696864fdc42c525ff73131323fcd8c
erdem <erdemozkol@gmail.com>
2017-07-30 00:35:51 +0100

    implemented new acceptance test scenarios

--- src/__tests__/plugins/register-form/acceptance/register_gp_form_test.js
+++ src/__tests__/plugins/register-form/acceptance/register_gp_form_test.js
@@ -7,14 +7,177 @@ const firstPractice = practiceLookup.getPractices()[0];
 
 Feature('GP Registration Form');
 
-Scenario('Test Start Page', (I) => {
+Scenario('Test all yes/no question with "no" answer on gp registration flow', (I) => {
   I.amOnPage('/');
   I.see('Apply to register with a GP practice');
   I.click(firstPractice.name);
   I.seeElement({css: '#start-button'});
   I.click('#start-button');
 
-  /*Do you know your NHS number?*/
-  I.click('label[for=input--nhs-number-known--2]');
+  I.see('Do you know your NHS number?');
+  I.click('label[data-label=No]');
   I.click('Continue');
+
+  I.see('What is your sex?');
+  I.click('label[data-label=Male]');
+  I.click('Continue');
+
+  I.see('What is your name?');
+  I.selectOption('Title','Mr');
+  I.fillField('First name', 'George');
+  I.fillField('Middle names', 'Alexander');
+  I.fillField('Last name', 'Louis');
+  I.click('Continue');
+
+  I.see('What is your date of birth?');
+  I.fillField('#input-day', '01');
+  I.fillField('#input-month','12');
+  I.fillField('#input-year','1976');
+  I.click('Continue');
+
+  I.see('What is your address?');
+  I.fillField('Postcode', 'SW1A1AA');
+  I.click('Continue');
+  I.click('Continue');  // this step has two part
+
+  I.see('Where were you born?');
+  I.selectOption('Country','United Kingdom');
+  I.fillField('Town or City', 'London');
+  I.click('Continue');
+
+  I.see(`How can ${firstPractice.name} contact you?`);
+  I.fillField('#input-bestPhone','07779998833');
+  I.fillField('#input-backUpPhone','07779998833');
+  I.fillField('#input-email','test@test.com');
+  I.click('Continue');
+
+  I.see('Have you served in the armed forces?');
+  I.click('label[data-label=No]');
+  I.click('Continue');
+
+  I.see('Are you already registered with a GP?');
+  I.click('label[data-label=No]');
+  I.click('Continue');
+
+  I.see('Are you taking any medication?');
+  I.click('label[data-label=No]');
+  I.click('Continue');
+
+  I.see('Do you have any allergies?');
+  I.click('label[data-label=No]');
+  I.click('Continue');
+
+  I.see('Have you ever had any of these conditions?');
+  I.click('label[data-label=Asthma]');
+  I.click('label[data-label=Cancer]');
+  I.click('label[data-label=Diabetes]');
+  I.click('Continue');
+
+  I.see('Check your details');
+  I.click('Send my details');
+
+  I.see(`Your application has been sent to ${firstPractice.name}`);
+});
+
+
+Scenario('Test all yes/no question with "yes" answer on gp registration flow', (I) => {
+  I.amOnPage('/');
+  I.see('Apply to register with a GP practice');
+  I.click(firstPractice.name);
+  I.seeElement({css: '#start-button'});
+  I.click('#start-button');
+
+  I.see('Do you know your NHS number?');
+  I.click('label[data-label=Yes]');
+  I.click('Continue');
+
+  I.see('What is your NHS number?');
+  I.fillField('NHS number', '943 476 5919');
+  I.click('Continue');
+
+  I.see('What is your sex?');
+  I.click('label[data-label=Female]');
+  I.click('Continue');
+
+  I.see('What is your name?');
+  I.selectOption('Title','Mrs');
+  I.fillField('First name', 'Elizabeth');
+  I.fillField('Middle names', 'Alexandra');
+  I.fillField('Last name', 'Mary');
+  I.click('Continue');
+
+  I.see('What is your date of birth?');
+  I.fillField('#input-day', '01');
+  I.fillField('#input-month','12');
+  I.fillField('#input-year','1976');
+  I.click('Continue');
+
+  I.see('What is your address?');
+  I.fillField('Postcode', 'SW1A1AA');
+  I.click('Continue');
+  I.click('Continue');  // this step has two part
+
+  I.see('Where were you born?');
+  I.selectOption('Country','United Kingdom');
+  I.fillField('Town or City', 'London');
+  I.click('Continue');
+
+  I.see(`How can ${firstPractice.name} contact you?`);
+  I.fillField('#input-bestPhone','07779998833');
+  I.fillField('#input-backUpPhone','07779998833');
+  I.fillField('#input-email','test@test.com');
+  I.click('Continue');
+
+  I.see('Have you served in the armed forces?');
+  I.click('label[data-label=Yes]');
+  I.click('Continue');
+
+  I.see('What was your service or staff number?');
+  I.fillField('#input-armedStaffNumber', '11111112233');
+  I.click('Continue');
+
+  I.see('When did you enlist?');
+  I.fillField('#input-day', '01');
+  I.fillField('#input-month', '01');
+  I.fillField('#input-year', '2010');
+  I.click('Continue');
+
+  I.see('Are you already registered with a GP?');
+  I.click('label[data-label=No]');
+  I.click('Continue');
+
+  /* todo mock elasticsearch module for gp-autocomplete component */
+
+  // I.see('What is the name and address of your current GP practice?');
+  // I.fillField('#gp-search', 'newland');
+  // I.waitForElement('.result', 10);
+  // I.click('.result');
+  // I.click('Continue');
+
+  I.see('Are you taking any medication?');
+  I.click('label[data-label=Yes]');
+  I.click('Continue');
+
+  I.see('List your regular medicines');
+  I.fillField('#input-medication', 'Lorem Ipsum is simply dummy text');
+  I.click('Continue');
+
+  I.see('Do you have any allergies?');
+  I.click('label[data-label=Yes]');
+  I.click('Continue');
+
+  I.see('List your allergies');
+  I.fillField('#input-allergies', 'Lorem Ipsum is simply dummy text');
+  I.click('Continue');
+
+  I.see('Have you ever had any of these conditions?');
+  I.click('label[data-label=Asthma]');
+  I.click('label[data-label=Cancer]');
+  I.click('label[data-label=Diabetes]');
+  I.click('Continue');
+
+  I.see('Check your details');
+  I.click('Send my details');
+
+  I.see(`Your application has been sent to ${firstPractice.name}`);
 });
