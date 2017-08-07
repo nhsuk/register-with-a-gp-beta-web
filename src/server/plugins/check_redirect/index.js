d45a12f192572bd9e4c87fcdabe5b23b2aa43c25
Tamas <tamas.marin@miqsolutions.co.uk>
2017-08-07 15:50:37 +0100

    cookie invalidation and redirect loop removed

--- src/server/plugins/check_redirect/index.js
+++ src/server/plugins/check_redirect/index.js
@@ -6,31 +6,35 @@ Plugin.register = function(server, options, next) {
   server.ext('onPreResponse', function (request, reply) {
     const url = request.url.path;
     const regIndex = url.indexOf('/register/');
-    if(regIndex > -1){
-      const slug = url.substr(regIndex + 10);
-      const jumpBack = request.state.fromSummaryTo;
-      if(jumpBack !== undefined ){
-        if(jumpBack !== slug){
-          if(stepDependency[jumpBack] !== undefined){
-            const dep = stepDependency[jumpBack];
-            let found = false;
-            dep.forEach((d) => {
-              if(d == slug){
-                found = true;
+//    const assetIndex = url.indexOf('/assets/');
+//    const staticIndex = url.indexOf('/static/');
+    const confirmIndex = url.indexOf('/register/confirm-details');
+    if(regIndex > -1) {
+      if(! (confirmIndex > -1)){
+        const slug = url.substr(regIndex + 10);
+        const jumpBack = request.state.fromSummaryTo;
+        if(jumpBack !== undefined ){
+          if(jumpBack !== slug){
+            if(stepDependency[jumpBack] !== undefined){
+              const dep = stepDependency[jumpBack];
+              let found = false;
+              dep.forEach((d) => {
+                if(d == slug){
+                  found = true;
+                }
+              });
+              if(!found ){
+                return reply
+                  .redirect('/' + request.params.practice + '/register/confirm-details');
               }
-            });
-            if(!found ){
+            } else {
               return reply
                 .redirect('/' + request.params.practice + '/register/confirm-details');
             }
-          } else {
-            return reply
-              .redirect('/' + request.params.practice + '/register/confirm-details');
           }
         }
       }
     }
-
     return reply.continue();
   });
   next();
