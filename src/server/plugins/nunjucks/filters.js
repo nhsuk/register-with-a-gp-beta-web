a6bf07aeb4894a7df0212fa7d7d145b930dd9d0f
erdem <erdemozkol@gmail.com>
2017-08-22 16:04:20 +0100

    nunjucks nl2br filter updates

--- src/server/plugins/nunjucks/filters.js
+++ src/server/plugins/nunjucks/filters.js
@@ -1,14 +1,20 @@
+import _ from 'lodash';
 import xss from 'xss';
 
 function getFilters(env) {
   return {
-    nl2br(str) {
-      var options = {
+    nl2br(line) {
+      if (Array.isArray(line)){
+        line = _.join(_.compact(_.map(line, x => _.trim(x))), ', ');
+      }
+
+      const options = {
         whiteList: {
           br: []
         }
       };
-      return xss(str.toString().replace(/\n/g, '<br>'), options);
+
+      return xss(line.toString().replace(/\n/g, '<br>'), options);
     },
     render(str){
       return env.renderString(str, this.getVariables());
