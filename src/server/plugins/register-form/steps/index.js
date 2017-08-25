282de1ad9dcd4c58b48a4d1f610e0f062351d74e
erdem <erdemozkol@gmail.com>
2017-08-25 13:41:21 +0100

    typo fixes

--- src/server/plugins/register-form/steps/index.js
+++ src/server/plugins/register-form/steps/index.js
@@ -62,7 +62,7 @@ const steps = [
   enterGpAddress,
   registeredAddressStep,
   registeredAddressDetailsStep,
-  registeredNameStep,x
+  registeredNameStep,
   registeredNameDetailsStep,
   currentMedicationStep,
   allergiesStep,
