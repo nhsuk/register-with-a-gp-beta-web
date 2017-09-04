f58486b0663ead05b0e87be09cf27cb27db538ed
Tamas <tamas.marin@miqsolutions.co.uk>
2017-09-04 15:52:37 +0100

    template value for posthandlerfactory

--- src/server/plugins/register-form/steps/common.js
+++ src/server/plugins/register-form/steps/common.js
@@ -213,9 +213,9 @@ export function postHandlerFactory(
   schema,
   prevSteps,
   nextSteps,
-  template = 'register-form/step',
   {
     beforeTemplate = undefined,
+  template = 'register-form/step',
     transformData = dataTransformer
   } = {}
 ) {
