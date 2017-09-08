8d0c3ed9b0df4ced38674ed5f659fcf3a821313d
erdem <erdemozkol@gmail.com>
2017-09-08 12:46:21 +0100

    updated acceptance/app-server.js

--- src/__tests__/plugins/register-form/acceptance/app-server.js
+++ src/__tests__/plugins/register-form/acceptance/app-server.js
@@ -2,9 +2,5 @@ import * as server from '../../../../server/index';
 
 module.exports = {
   bootstrap: server.start,
-  teardown: () => {
-    setTimeout(() => {
-      process.exit();
-    }, 1000);
-  },
+  teardown: process.exit,
 };
