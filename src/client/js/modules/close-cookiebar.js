6f48e996e8f547db6839afc46596f4f3c46280dd
Harshada S. Thakur <Harshad12392@Mastek.com>
2017-09-08 11:43:00 +0100

    close cookie bar and dropdown apperance issue

---
+++ src/client/js/modules/close-cookiebar.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+
+class closeCookieBar {
+	init() {
+		$("#global-cookies-banner").on('click',function(){
+			$("#global-cookies-banner").hide();
+		})
+	}
+
+}
+
+module.exports = new closeCookieBar();
