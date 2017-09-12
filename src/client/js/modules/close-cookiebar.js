0e7b5eab5945c9d57ba44e8eb0448233ce3a9683
Harshada S. Thakur <Harshad12392@Mastek.com>
2017-09-12 11:43:04 +0100

    expiration time set for cookie bar close, and close icon changes

--- src/client/js/modules/close-cookiebar.js
+++ src/client/js/modules/close-cookiebar.js
@@ -2,8 +2,17 @@ const $ = require('jquery');
 
 class closeCookieBar {
   init() {
-    $('#global-cookies-banner').on('click',function(){
-      $('#global-cookies-banner').hide();
+  	if(document.cookie.indexOf('hideCookieBar=true')>=0){
+  		$('#global-cookies-banner').empty();
+  	}
+
+    $('.close').on('click',function(){
+    	let d = new Date();
+		d.setTime( d.getTime()+ 30*24*60*60*1000);
+		let expireTime = 'expires='+d.toUTCString();
+
+    	$('#global-cookies-banner').empty();
+      	document.cookie="hideCookieBar="+true +";"+expireTime;
     });
   }
 
