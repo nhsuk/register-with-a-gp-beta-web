ee559f0c7a0916cbbd5423a248b76936f38198ee
erdem <erdemozkol@gmail.com>
2017-09-12 11:51:31 +0100

    fixed eslint warnings

--- src/client/js/modules/close-cookiebar.js
+++ src/client/js/modules/close-cookiebar.js
@@ -2,17 +2,17 @@ const $ = require('jquery');
 
 class closeCookieBar {
   init() {
-  	if(document.cookie.indexOf('hideCookieBar=true')>=0){
-  		$('#global-cookies-banner').empty();
-  	}
+    if(document.cookie.indexOf('hideCookieBar=true')>=0){
+      $('#global-cookies-banner').empty();
+    }
 
     $('.close').on('click',function(){
-    	let d = new Date();
-		d.setTime( d.getTime()+ 30*24*60*60*1000);
-		let expireTime = 'expires='+d.toUTCString();
+      let d = new Date();
+      d.setTime( d.getTime()+ 30*24*60*60*1000);
+      let expireTime = 'expires='+d.toUTCString();
 
-    	$('#global-cookies-banner').empty();
-      	document.cookie="hideCookieBar="+true +";"+expireTime;
+      $('#global-cookies-banner').empty();
+      document.cookie='hideCookieBar='+true +';'+expireTime;
     });
   }
 
