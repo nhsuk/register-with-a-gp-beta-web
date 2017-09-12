8e704baf7ec46f5d49bee5e7b4a526f37809a223
erdem <erdemozkol@gmail.com>
2017-09-12 15:50:52 +0100

    implemented test server start and stop functions

--- src/__tests__/plugins/register-form/acceptance/app-server.js
+++ src/__tests__/plugins/register-form/acceptance/app-server.js
@@ -1,12 +1,47 @@
-import * as server from '../../../../server/index';
+let server;
+let _runningInstance;
 
-function stopServer(){
-  setTimeout(function () {
-    process.exit();
-  }, 2000);
+class ServerInstanceUndefined {
+  constructor(message = 'Server Instance is Undefined') {
+    this.name = 'ServerInstanceUndefined';
+    this.message = message;
+    this.stack = (new Error()).stack;
+  }
 }
 
+function startTestServer(done) {
+  return new Promise((resolve) => {
+    server = require('../../../../server/');
+    process.env.PORT = '6666';
+    server.start().then((instance) => {
+      _runningInstance = instance;
+      instance.start((err) => {
+        if (err) {
+          throw err;
+        }
+        instance.log('info', 'Server running at: ' + instance.info.uri);
+        resolve();
+        done();
+      });
+    });
+  });
+}
+
+function stopTestServer(done) {
+  return new Promise((resolve) => {
+    if (_runningInstance) {
+      server.stop(_runningInstance).then(function () {
+        resolve();
+      });
+    } else {
+      throw ServerInstanceUndefined();
+    }
+    done();
+  });
+}
+
+
 module.exports = {
-  bootstrap: server.start,
-  teardown: stopServer
+  bootstrap: startTestServer,
+  teardown: stopTestServer,
 };
