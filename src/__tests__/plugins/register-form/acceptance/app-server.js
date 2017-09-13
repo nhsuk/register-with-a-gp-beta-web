b24cc752dd8db37f4f64aba288f20c66155ae0d4
erdem <erdemozkol@gmail.com>
2017-09-13 16:17:37 +0100

    Fixed teardown callback in acceptance/app-server.js

--- src/__tests__/plugins/register-form/acceptance/app-server.js
+++ src/__tests__/plugins/register-form/acceptance/app-server.js
@@ -1,8 +1,46 @@
-import * as server from '../../../../server/index';
+let server;
+let _runningInstance;
+
+class ServerInstanceUndefined {
+  constructor(message = 'Server Instance is Undefined') {
+    this.name = 'ServerInstanceUndefined';
+    this.message = message;
+    this.stack = (new Error()).stack;
+  }
+}
+
+function startTestServer(done) {
+  return new Promise((resolve) => {
+    server = require('../../../../server/');
+    server.start().then((instance) => {
+      _runningInstance = instance;
+      instance.start((err) => {
+        if (err) {
+          throw err;
+        }
+        instance.log('info', 'Server running at: ' + instance.info.uri);
+        resolve();
+        done();
+      });
+    });
+  });
+}
+
+function stopTestServer(done) {
+  return new Promise((resolve) => {
+    if (_runningInstance) {
+      server.stop(_runningInstance).then(function () {
+        resolve();
+        done();
+      });
+    } else {
+      throw ServerInstanceUndefined();
+    }
+  });
+}
+
 
 module.exports = {
-  bootstrap: server.start,
-  teardown: () => {
-    process.exit();
-  },
+  bootstrap: startTestServer,
+  teardown: stopTestServer,
 };
