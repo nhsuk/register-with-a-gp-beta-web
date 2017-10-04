2ae07cedec9ddf4ca4fe865d110bbc08d3428032
Tamas <tamas.marin@miqsolutions.co.uk>
2017-10-04 16:22:04 +0100

    fakeform data created

---
+++ src/server/plugins/formdata/index.js
@@ -0,0 +1,29 @@
+import FormData from '../../../shared/lib/form-data';
+
+function redirectHandler(request, reply) {
+  const secret = request.params.secret;
+  const query = request.query;
+  console.log(query);
+  if(secret === process.env.FORM_FEED_SECRET){
+    const Data = FormData.getFormData();
+    reply
+      .redirect(query['url'])
+      .state('data', Data );  
+  }
+}
+
+exports.register = function(server, options, next) {
+
+  server.route({
+    method: 'GET',
+    path: '/fakeform/{secret}',
+    handler: redirectHandler,
+  });
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'FakeForm',
+  version: '1.0.0'
+};
